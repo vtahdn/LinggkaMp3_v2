bfa73956772ecbedb2be79618df0d4560354d4b0
Viet Asc <vtahdn@gmail.com>
2018-12-04 13:44:14 +0700

    Initial Commit

---
+++ LinggkaMp3_v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinggkaMp3_v2
+//
+//  Created by Viet Asc on 12/4/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
